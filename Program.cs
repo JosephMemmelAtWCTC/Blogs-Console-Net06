6fe73ce94b8590b4d5fa686d8a0e6e5db70be369
42null <averyparanoiduser@gmail.com>
2023-11-21 12:53:18 -0600

    Delate blog functationality

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ logger.Info("Program started");
 
 string[] MAIN_MENU_OPTIONS_IN_ORDER = { enumToStringMainMenuWorkaround(MAIN_MENU_OPTIONS.Display_All_Blogs),
                                         enumToStringMainMenuWorkaround(MAIN_MENU_OPTIONS.Add_Blog),
+                                        enumToStringMainMenuWorkaround(MAIN_MENU_OPTIONS.Deleate_Blog),
                                         enumToStringMainMenuWorkaround(MAIN_MENU_OPTIONS.Create_Post),
                                         enumToStringMainMenuWorkaround(MAIN_MENU_OPTIONS.Display_Posts),
                                         enumToStringMainMenuWorkaround(MAIN_MENU_OPTIONS.Exit)};
@@ -174,7 +175,17 @@ do
     // For each Post, display the Blog name, Post title and Post content
 
     }
-
+    else if (menuCheckCommand == enumToStringMainMenuWorkaround(MAIN_MENU_OPTIONS.Deleate_Blog))
+    {
+        Blog selectedBlog = selectBlog("Please select a blog to delete: ");
+        if (selectedBlog != null)
+        {
+            var db = new BloggingContext();
+            // Deleate the blog
+            db.DeleteBlog(selectedBlog);
+            logger.Info($"Blog (id: {selectedBlog.BlogId}) deleted");
+        }
+    }
     else
     {
         logger.Warn("That menu option is not available, please try again.");
@@ -231,6 +242,7 @@ string enumToStringMainMenuWorkaround(MAIN_MENU_OPTIONS mainMenuEnum)
         MAIN_MENU_OPTIONS.Exit => "Quit program",
         MAIN_MENU_OPTIONS.Display_All_Blogs => $"Display all blogs", // on file (display max amount is {UserInteractions.PRINTOUT_RESULTS_MAX_TERMINAL_SPACE_HEIGHT / 11:N0})"
         MAIN_MENU_OPTIONS.Add_Blog => "Add Blog",
+        MAIN_MENU_OPTIONS.Deleate_Blog => "Deleate a blog",
         MAIN_MENU_OPTIONS.Create_Post => "Create New Post",
         MAIN_MENU_OPTIONS.Display_Posts => "Display Posts",
         _ => "ERROR_MAIN_MENU_OPTION_DOES_NOT_EXIST"
@@ -243,5 +255,6 @@ public enum MAIN_MENU_OPTIONS
     Display_All_Blogs,
     Add_Blog,
     Create_Post,
-    Display_Posts
+    Display_Posts,
+    Deleate_Blog
 }
